0a1e0d5f34dfe882b40b248c385b87e652d0a677
Thomas Ricouard <ricouard77@gmail.com>
2015-01-24 20:24:29 +0100

    Switch to mongoose batch insert

--- providers/jobProvider.js
+++ providers/jobProvider.js
@@ -18,7 +18,7 @@ JobProvider = function() {
 JobProvider.prototype.seed = function() {
 	Job.remove(function (err) {if (err) console.log ('Error on remove!')});
 
-	var gloseJob = new Job ({
+	var gloseJob = {
 		logo: "/images/glose.png",
 		logoStyle: "margin-top: 10px; margin-right: 25px; margin-left: 15px",
 		company: "Glose",
@@ -26,9 +26,9 @@ JobProvider.prototype.seed = function() {
 		url: "http://www.glose.com",
 		time: "Setember 2013 to now",
 		order: "0"
-	});
+	};
 
-	var mySeeenJob = new Job ({
+	var mySeeenJob = {
 		logo: "/images/myseeen.png",
 		logoStyle: "margin-top: 10px; margin-right: 25px; margin-left: 15px",
 		company: "MySeeen",
@@ -36,9 +36,9 @@ JobProvider.prototype.seed = function() {
 		url: "http://www.myseeenapp.com",
 		time: "February 2012 to now",
 		order: "1"
-	});
+	};
 
-	var googleJob = new Job ({
+	var googleJob = {
 		logo: "/images/google.png",
 		logoStyle: "margin-top: 17px; margin-right: 10px",
 		company: "Google",
@@ -46,9 +46,9 @@ JobProvider.prototype.seed = function() {
 		url: "http://google.com",
 		time: "February 2013 to July 2013",
 		order: "2"
-	});
+	};
 
-	var wayzupJob = new Job ({
+	var wayzupJob = {
 		logo: "/images/wayzup-logo.png",
 		logoStyle: "margin-top: 10px; margin-right: 10px",
 		company: "Wayz-Up",
@@ -56,9 +56,9 @@ JobProvider.prototype.seed = function() {
 		url: "http://www.wayz-up.com",
 		time: "June 2012 to May 2013",
 		order: "3"
-	});
+	};
 
-	var sageJob1 = new Job ({
+	var sageJob1 = {
 		logo: "/images/sage-logo.png",
 		logoStyle: "margin-top: 15px; margin-right: 10px;",
 		company: "Sage",
@@ -66,9 +66,9 @@ JobProvider.prototype.seed = function() {
 		url: "http://sage.fr",
 		time: "September 2011 to June 2012",
 		order: "4"
-	});
+	};
 
-	var ravenJob = new Job ({
+	var ravenJob = {
 		logo: "/images/raven-logo.png",
 		logoStyle: "margin-top: 15px; margin-right: 30px",
 		company: "Raven",
@@ -76,9 +76,9 @@ JobProvider.prototype.seed = function() {
 		url: "http://raven.io",
 		time: "April 2011 to May 2012",
 		order: "5"
-	});
+	};
 
-	var sageJob2 = new Job ({
+	var sageJob2 = {
 		logo: "/images/sage-logo.png",
 		logoStyle: "margin-top: 15px; margin-right: 10px",
 		company: "Sage",
@@ -86,15 +86,15 @@ JobProvider.prototype.seed = function() {
 		url: "http://sage.fr",
 		time: "2009 to August 2011",
 		order: "6"
-	});
+	};
 
-	gloseJob.save(function (err) {if (err) console.log ('Error on save!')});
-	mySeeenJob.save(function (err) {if (err) console.log ('Error on save!')});
-	googleJob.save(function (err) {if (err) console.log ('Error on save!')});
-	wayzupJob.save(function (err) {if (err) console.log ('Error on save!')});
-	sageJob1.save(function (err) {if (err) console.log ('Error on save!')});
-	ravenJob.save(function (err) {if (err) console.log ('Error on save!')});
-	sageJob2.save(function (err) {if (err) console.log ('Error on save!')});
+	var jobs = [gloseJob, mySeeenJob, googleJob, wayzupJob, sageJob1, sageJob2, ravenJob];
+
+	Job.collection.insert(jobs, function(error, docs) {
+		if (error) {
+			console.log('Save error');
+		}
+	});
 };
 
 JobProvider.prototype.findall = function(callback) {
